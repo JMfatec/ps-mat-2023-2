8938d0cc05c699ef9815d0e2645fa9a32d0d151b
João Marcos <124882613+JMfatec@users.noreply.github.com>
2023-08-09 13:58:29 +0000

    (09/08) Configuração inicial do projeto

---
+++ back-end/src/app.js
@@ -0,0 +1,21 @@
+import express, { json, urlencoded } from "express";
+import cookieParser from "cookie-parser";
+import logger from "morgan";
+
+import indexRouter from "./routes/index.js";
+import usersRouter from "./routes/users.js";
+
+//Importa o cliente do prisma para fazer a conexão com o banco de dados
+import prisma from "./database/client.js";
+
+const app = express();
+
+app.use(logger("dev"));
+app.use(json());
+app.use(urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+
+export default app;
